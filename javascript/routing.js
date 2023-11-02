73e8016c3f7308f8398497f307ec10c6da3a686c
Fineas Dolha <114675669+fineasdolha@users.noreply.github.com>
2023-11-02 16:57:22 +0100

    update
    update styles
    update language
    update itinerary

--- javascript/routing.js
+++ javascript/routing.js
@@ -1,22 +1,62 @@
 
+let routing;
+let divParent;
+let resetButton;
 
 function changelanguage () { 
     let languageSelection = document.querySelector('input[name=language]:checked').value; 
     return languageSelection;
 }
 
+
+
+
 function createItinerary() {  
-    document.getElementsByClassName('leaflet-routing-container').innerHTML ='';
-     
-    L.Routing.control({
+    
+    routing = L.Routing.control({
+        
         waypoints: [
             L.latLng(),
             L.latLng()
         ],
         routeWhileDragging: true,
         geocoder: L.Control.Geocoder.nominatim(),
-        language : changelanguage()
-    }).addTo(map);
-    
-    
+        language : changelanguage(),
+        showAlternatives: true,
+        altLineOptions: {
+            styles: [
+                {color: 'gray', opacity: 0.8, weight: 6}
+                
+            ]
+        },
+        lineOptions: {
+            styles: [
+                {color: 'blue', opacity: 0.8, weight: 6}
+                
+            ]
+        }
+         
+        
+    })
+    .on('routesfound', function(e) {
+        var routes = e.routes;
+        
+        // alert('Found ' + routes.length + ' route(s).');
+    })
+    .on('routeselected', function(e) {
+        var route = e.route;
+        console.log(route);
+
+    })
+        .addTo(map);
+        divParent = document.querySelector('.leaflet-routing-container');
+        resetButton = document.createElement('span');
+        resetButton.classList.add('leaflet-routing-collapse-btn')
+        resetButton.addEventListener('click', () => {resetRoute()});
+        divParent.classList.add('leaflet-routing-collapsible');
+        divParent.append(resetButton);
+}
+
+function resetRoute () {
+    routing.spliceWaypoints(0, 2);
 }
